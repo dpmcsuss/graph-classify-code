3aa9a449f827094dc6506482f788dbcea52ee2ab
jovo <joshuav@jhu.edu>
2011-01-06 09:34:57 -0500

    fucked up everthing, reverting back

--- meta/classify_par.m
+++ meta/classify_par.m
@@ -14,25 +14,27 @@ function [Lhats xval] = classify_par(As,constants,xval,subspace,preds)
 %   xval:       xval structure after changing it
 
 %% ensure that xval fields are set
-if ~isfield(xval,'s0_trn'),
-    disp(['s0 = ' num2str(constants.s0)])
-    xval.s0_trn = input('\s0_trn [positive integer]: ');
-end
-if ~isfield(xval,'s1_trn'),
-    disp(['s1 = ' num2str(constants.s1)])
-    xval.s1_trn = input('\s1_trn [positive integer]: ');
-end
+% if ~isfield(xval,'s0_trn'),
+%     disp(['s0 = ' num2str(constants.s0)])
+%     xval.s0_trn = input('\s0_trn [positive integer]: ');
+% end
+% if ~isfield(xval,'s1_trn'),
+%     disp(['s1 = ' num2str(constants.s1)])
+%     xval.s1_trn = input('\s1_trn [positive integer]: ');
+% end
+
+% if num_iters is not specified, do leave-one-out cross-validation
 if ~isfield(xval,'num_iters'),
-    xval.num_iters = choose(constants.s0,xval.s0_trn)*choose(constants.s1,xval.s1_trn);
+    xval.num_iters = constants.s; %choose(constants.s0,xval.s0_trn)*choose(constants.s1,xval.s1_trn);
 end
 
 
 
 %% run classifiers
 Lstats_tmp(1:xval.num_iters)=struct('avg',nan,'sem',nan);
-parfor i=1:xval.num_iters
+for i=1:xval.num_iters
     if mod(i,100)==0, disp(['repeat # ' num2str(i)]), end
-    [Atrn Gtrn Atst Gtst inds] = crossvalprep2(As,constants,xval,i); % seperate data into training and testing sets
+    [Atrn Gtrn Atst Gtst inds] = crossvalprep3(As,constants,xval,i); % seperate data into training and testing sets
     
     % naive_bayes classifiers
     if ~isempty(subspace)
